b097c63c4d4ffad0aa6bf0686547ce6f6edcdd38
E Kindangen <ekindangen@gmail.com>
2026-07-15 11:19:37 +0700

    Fix development and production API routing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://54.206.224.166",
-        changeOrigin: true,
-      },
-    },
-  },
+
 });
